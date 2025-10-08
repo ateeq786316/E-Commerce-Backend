a92bbab692721c9a9e4d2f9a418f70264bf854a6
ATEEQ UR REHMAN <ateeq786316@gmail.com>
2025-10-08 16:43:00 +0500

    auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    })
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignupDto } from './dto/signup.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService,
+    ){}
+
+    async signup(signupDto: SignupDto){
+        const existingUser = await this.prisma.user.findUnique({
+            where: {email: signupDto.email},
+        });
+        if(existingUser){
+            throw new HttpException('User already exists', HttpStatus.CONFLICT);
+        }
+
+        const hashedPassword = await bcrypt.hash(signupDto.password, 10);
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: signupDto.email,
+                name: signupDto.name,
+                password: hashedPassword,
+            },
+        });
+
+        const payload = { email: user.email, sub: user.id };
+        const accessToken = await this.jwtService.signAsync(payload, { expiresIn: '15m' });
+        const refreshToken = await this.jwtService.signAsync(payload, { expiresIn: '7d' });
+
+        
+    }
+
+
+}
