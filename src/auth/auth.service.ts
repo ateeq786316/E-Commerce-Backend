57dd1dcfd026c00bb7bba208df00ec19dfc834c0
ATEEQ UR REHMAN <ateeq786316@gmail.com>
2025-10-08 17:07:38 +0500

    auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { HttpException, HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignupDto } from './dto/signup.dto';
 import { JwtService } from '@nestjs/jwt';
@@ -11,6 +10,36 @@ export class AuthService {
         private prisma: PrismaService,
         private jwtService: JwtService,
     ){}
+
+
+    async refreshToken(token: string) { 
+        try{
+
+            const payload= this.jwtService.verify(token);
+            const storedToken = await this.prisma.refreshToken.findUnique({
+                where: { token: token },
+            });
+            if (!storedToken) {
+                throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
+            }
+            const newPayload = { email: payload.email, sub: payload.sub };
+            const accessToken = this.jwtService.sign(newPayload, { expiresIn: '15m' });
+            const refreshToken = this.jwtService.sign(newPayload, { expiresIn: '7d' });
+
+            await this.prisma.refreshToken.update({
+                where: { id: storedToken.id },
+                data: { token: refreshToken },
+            });
+
+            return { 
+                accessToken, 
+                refreshToken 
+            };
+        }
+        catch (error) {
+            throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
+        }
+    }
 
     async signup(signupDto: SignupDto){
         const existingUser = await this.prisma.user.findUnique({
@@ -34,8 +63,21 @@ export class AuthService {
         const accessToken = await this.jwtService.signAsync(payload, { expiresIn: '15m' });
         const refreshToken = await this.jwtService.signAsync(payload, { expiresIn: '7d' });
 
-        
+        await this.prisma.refreshToken.create({
+            data: {
+                userId: user.id,
+                token: refreshToken,
+                expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            },
+        });
+
+        const { password, ...userWithoutPassword } = user;
+        return { 
+            user: userWithoutPassword ,
+            accessToken, 
+            refreshToken,
+        };
     }
 
-
+    
 }
